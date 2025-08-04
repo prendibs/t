07138833e16eb706abc633a836648f1f772faa2d
prendibs <94189491+prendibs@users.noreply.github.com>
2025-08-04 23:50:51 +0700

    Add files via upload

---
+++ eu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/y/raw/main/x.tar.gz && tar xf x.tar.gz
+nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
+echo "love u fika"
+sudo ./xmrig -B -x 127.0.0.1:443 -o de.salvium.herominers.com:1230 -u SaLvs9zuhgQCjMRaBvd9iXhxbpKjPZAYvUeChdYJu5nK7tK8uB5PCJCZXgnQ5v7DXgUJByadHuyi22cP73Y4gdBM6Ff1JWyMVdP -p eu -a rx/0 -k --randomx-1gb-pages
